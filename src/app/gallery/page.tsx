177d41b678efdd6d06a139f971c3217be7067437
KillerLns <Linus.musshafen@gmail.com>
2024-06-07 18:05:07 +0200

    gallery grid-cols-4

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -6,11 +6,10 @@ import Image from "next/image";
 
 export default function Page() {
     const [messages, setMessages] = useState<MessageType[]>([]);
-    const width = 200;
 
     useEffect(() => {
         async function init() {
-            const msgs = (await getMessages()).reverse()
+            const msgs = (await getMessages()).reverse();
             setMessages(msgs);
         }
         init();
@@ -19,21 +18,21 @@ export default function Page() {
     return (
         <div className="flex flex-col h-[calc(91vh)]">
             <div className="overflow-auto p-0">
-                <div className="flex flex-wrap">
-                    {messages.map((message) => (
+                <div className="grid grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-1">
+                    {messages.map((message) =>
                         message.image_upscale_urls.map((link, index) => (
-                            <div key={`${message.id}-${index}`} className="mb-1 mr-1" style={{ width: width, height: width, overflow: 'hidden' }}>
-                                <div style={{ width: '100%', height: '100%', position: 'relative' }}>
-                                    <Image
-                                        src={link}
-                                        alt={message.prompt}
-                                        layout="fill"
-                                        objectFit="cover"
-                                    />
-                                </div>
+                            <div key={`${message.id}-${index}`} className="relative">
+                                <Image
+                                    src={link}
+                                    alt={message.prompt}
+                                    layout="responsive"
+                                    width={1}
+                                    height={1}
+                                    objectFit="cover"
+                                />
                             </div>
                         ))
-                    ))}
+                    )}
                 </div>
             </div>
         </div>
